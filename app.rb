adf1bd52ea2a33c8a1a322e3ecba9a2efa57d5f0
Kate <katepdonahue@gmail.com>
2014-01-15 14:53:22 -0500

    Add some theoretical routes

--- app.rb
+++ app.rb
@@ -31,4 +31,28 @@ class App < Sinatra::Application
     haml :show
   end
 
+  get '/:id' do
+    @spacecat = Spacecat.find(:id)
+    haml :show
+  end
+
+  get '/:id/edit' do
+    @spacecat = Spacecat.find(:id)
+    haml :edit
+  end
+
+  post '/:id' do
+    @spacecat = Spacecat.find(:id)
+    @spacecat.name = params["name"]
+    @spacecat.planet = params["planet"]
+    @spacecat.tagline = params["tagline"]
+    @spacecat.superpower = params["superpower"]
+    haml :show
+  end
+
+  delete '/:id' do
+    Spacecat.find(:id).destroy
+    haml :index
+  end
+
 end
