af70297c44a7465580f9147eb6469ab8dba459b5
Kate <katepdonahue@gmail.com>
2014-01-15 13:42:01 -0500

    Create a spacecats table and run migration

--- db/migrate/20140115183944_spacecats.rb
+++ db/migrate/20140115183944_spacecats.rb
@@ -1,7 +1,10 @@
 class Spacecats < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table :spacecats do |t|
+      t.string :name
+      t.string :planet
+      t.string :tagline
+      t.string :superpower
+    end
   end
 end
